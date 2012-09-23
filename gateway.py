4c24397de4e14c4a5bc82d77de37f0f17a32d4aa
Josh Kearney <josh@jk0.org>
2012-09-23 22:18:59 +0000

    Don't send responses if we're the one sending.

--- gateway.py
+++ gateway.py
@@ -70,11 +70,13 @@ class CampfireXmppGateway(sleekxmpp.ClientXMPP):
         self.send_message(self.xmpp_recipient, message)
 
     def campfire_process_incoming(self, message):
+        if user == self.cf_real_name:
+            return
+
         user = None
         if message.user:
             user = message.user.name
 
-        response = None
         if message.is_joining():
             response = "[%s entered the room]" % user
         elif message.is_leaving():
@@ -91,9 +93,6 @@ class CampfireXmppGateway(sleekxmpp.ClientXMPP):
                                                  message.upload["url"])
         elif message.is_topic_change():
             response = "[%s changed topic to '%s']" % (user, message.body)
-
-        if user == self.cf_real_name:
-            response = ""
 
         self.campfire_send_message(response)
 
